649cda3006f598e4afd4f20c4debf1baa576845f
zhouxudong <zhouxudong168@126.com>
2019-08-07 18:59:32 +0800

    新增test case

--- Operate/example.py
+++ Operate/example.py
@@ -10,3 +10,14 @@
 """
 每个page里面包含的业务的实现逻辑方法
 """
+import unittest
+from Pages.pagexxx import Login
+from Pages.login_location import username_location, password_location, login_location
+
+
+class Testlogin(unittest.TestCase):
+    def test_login(self):
+        l = Login()
+        l.input_userinfo(username_location)
+        l.input_userinfo(password_location)
+        l.login_button(login_location)
--- Pages/pagexxx.py
+++ Pages/pagexxx.py
@@ -13,9 +13,18 @@
 from Helper.APPdriver import AppDriver
 from Helper.appLocations import AppLocation
 from Pages.login_location import username_location, password_location, login_location
+from time import sleep
 
 
-def input_userinfo(inputstring):
-    dri = AppDriver
-    driver = AppDriver.driver()
-    driver.find_element_by_xpath()
+class Login:
+    def __init__(self):
+        dri = AppDriver
+        self.driver = dri.driver()
+
+    def input_userinfo(self, element_location, inputstring):
+        sleep(5)
+        self.driver.find_element_by_xpath(element_location).send_keys(inputstring)
+
+    def login_button(self, element_location):
+        sleep(5)
+        self.driver.find_element_by_id(element_location).click()
